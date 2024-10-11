f058e33ea88940f5562fbe793a80773cc548c9e6
dev24mxpertz <dev24.mxpertz@gmail.com>
2024-10-11 18:00:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import ContainerImage from "./assests/image.avif";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
-function App() {
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  const containerRef = useRef(null);
+  const imageRef = useRef(null);
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    const imageRevealRule = CSSRulePlugin.getRule(".img-container::after");
+    tl.to(containerRef.current, { css: { visibility: "visible" }, duration: 0 })
+      .to(imageRevealRule, { width: "0%", ease: "power2.inOut", duration: 1.4 })
+      .from(imageRef.current, {
+        scale: 1.6,
+        ease: "power2.inOut",
+        duration: 1.4,
+        delay: -1.4,
+      });
+  }, [tl]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="main">
+        <div ref={containerRef} className="container">
+          <div className="img-container">
+            <img src={ContainerImage} alt="ContainerImage" ref={imageRef} />
+          </div>
+        </div>
+      </section>
+    </>
   );
-}
+};
 
 export default App;
