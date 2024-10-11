c57384e0e4c680c562c396028c73c57fc3d3c426
dev24mxpertz <dev24.mxpertz@gmail.com>
2024-10-11 18:08:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import CSSRulePlugin from "gsap/CSSRulePlugin";
 import ContainerImage from "./assests/image.avif";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger , CSSRulePlugin);
 
 const App = () => {
   const containerRef = useRef(null);
   const imageRef = useRef(null);
-
   const tl = gsap.timeline();
 
   useEffect(() => {
+    const container = containerRef.current;
     const imageRevealRule = CSSRulePlugin.getRule(".img-container::after");
-    tl.to(containerRef.current, { css: { visibility: "visible" }, duration: 0 })
-      .to(imageRevealRule, { width: "0%", ease: "power2.inOut", duration: 1.4 })
+      container.style.visibility = "visible";
+    // tl.to(containerRef.current, { css: { visibility: "visible" }, duration: 0 })
+      tl.to(imageRevealRule, { width: "0%", ease: "power2.inOut", duration: 1.4 })
       .from(imageRef.current, {
         scale: 1.6,
         ease: "power2.inOut",
